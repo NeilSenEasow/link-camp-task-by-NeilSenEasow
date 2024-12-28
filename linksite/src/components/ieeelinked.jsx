7f162ef514e3ef956b7f97e358115549efeed6e7
Shibu Easow <shibueasow@Shibus-MacBook-Air.local>
2024-12-29 00:18:36 +0530

    Locofy import

---
+++ linksite/src/components/ieeelinked.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IeeeLinked = () => {
+  return <div>IeeeLinked</div>;
+};
+
+export default IeeeLinked;
---
+++ linksite/src/components/officebearers.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OfficeBearers = () => {
+  return <div>OfficeBearers</div>;
+};
+
+export default OfficeBearers;
